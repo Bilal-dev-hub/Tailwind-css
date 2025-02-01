335d77c54fc4c74da2490bc5d4ad61d943f4176d
M.Bilal <m.bilal00989@gmail.com>
2025-02-01 14:25:20 +0500

    New project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode:"class",
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      
+      width: {
+        '128': '32rem', 
+        '144': '36rem',
+        'custom': '50vw'
+      }
+     },
+  },
+  plugins: [],
+}
